93303d2305ed3a3cf5ed1009b4964a66cc07605a
Joshua-Gabriel Camacho <shua.camacho@gmail.com>
2022-12-09 00:14:23 -0500

    Update logic to fit DOM
    Not complete yet, needs to still be hashed out.

--- logic.js
+++ logic.js
@@ -1,52 +1,86 @@
-
 //global variables
+const selections = [
+    {
+        input: 'rock',
+        beats: 'scissors',
+    },
+    {
+        input: 'scissors',
+        beats: 'paper',
+    },
+    {
+        input: 'paper',
+        beats: 'rock',
+    }
+]
 
 
 //random choice generator for computer
-function getComputerChoice() {
-    let randomNumber = Math.floor(Math.random() * 3)
 
-    if (randomNumber == 0) {
-        return "rock"
-    } else if (randomNumber == 1) {
-        return "paper"
-    } else {
-        return "scissors"
-    }
+function getComputerChoice() {
+    const selectIndex = Math.floor(Math.random() * selections.length);
+    return selections[selectIndex]
 }
 
-//player selector from buttons
+console.log(getComputerChoice())
 
-const buttons = document.querySelectorAll('[data-selection]')
+//click even based on what u click
 
-buttons.forEach(buttons => {
-    buttons.addEventListener('click', e => {
-        playerSelection = buttons.dataset.selection
-        console.log(playRound(playerSelection, getComputerChoice()))
-        console.log(getComputerChoice())
+const buttons = document.querySelectorAll('button')
+
+buttons.forEach((button) => {
+    button.addEventListener('click', e => {
+        const userInput = button.dataset.selection
     })
 })
 
 
-//rounds of RPS with return statements on W/L/T
-function playRound(playerSelection, computerSelection) {
+
+// function getComputerChoice() {
+//     let randomNumber = Math.floor(Math.random() * 3)
+
+//     if (randomNumber == 0) {
+//         return "rock"
+//     } else if (randomNumber == 1) {
+//         return "paper"
+//     } else {
+//         return "scissors"
+//     }
+// }
+
+// //player selector from buttons
+
+// const buttons = document.querySelectorAll('[data-selection]')
+
+// buttons.forEach(buttons => {
+//     buttons.addEventListener('click', e => {
+//         playerSelection = buttons.dataset.selection
+//         console.log(playRound(playerSelection, getComputerChoice()))
+//         console.log(getComputerChoice())
+//     })
+// })
+
+
+
+// //rounds of RPS with return statements on W/L/T
+// function playRound(playerSelection, computerSelection) {
     
 
-    if (playerSelection === computerSelection) {
-        return "Nobody wins; it\'s a tie!"
-    } else if (playerSelection === "rock" && computerSelection === "scissors") {
-        return "You win!"
-    } else if (playerSelection === "rock" && computerSelection === "paper") {
-        return "You lose!"
-    } else if (playerSelection === "paper" && computerSelection === "rock") {
-        return "You win!"
-    } else if (playerSelection === "paper" && computerSelection === "scissors") {
-        return "You lose!"
-    } else if (playerSelection === "scissors" && computerSelection === "paper") {
-        return "You win!"
-    } else if (playerSelection === "scissors" && computerSelection === "rock") {
-        return "You lose!"
-    } else {
-        return "What happened..."
-    }
-}
+//     if (playerSelection === computerSelection) {
+//         return "Nobody wins; it\'s a tie!"
+//     } else if (playerSelection === "rock" && computerSelection === "scissors") {
+//         return "You win!"
+//     } else if (playerSelection === "rock" && computerSelection === "paper") {
+//         return "You lose!"
+//     } else if (playerSelection === "paper" && computerSelection === "rock") {
+//         return "You win!"
+//     } else if (playerSelection === "paper" && computerSelection === "scissors") {
+//         return "You lose!"
+//     } else if (playerSelection === "scissors" && computerSelection === "paper") {
+//         return "You win!"
+//     } else if (playerSelection === "scissors" && computerSelection === "rock") {
+//         return "You lose!"
+//     } else {
+//         return "What happened..."
+//     }
+// }
