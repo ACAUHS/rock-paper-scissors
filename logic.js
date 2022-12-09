942316b460ad3091abc6ee0a07e87502c761a88b
Joshua-Gabriel Camacho <shua.camacho@gmail.com>
2022-12-09 04:19:05 -0500

    Integrate eventListener to the three selections

--- logic.js
+++ logic.js
@@ -1,86 +1,43 @@
 //global variables
-const selections = [
-    {
-        input: 'rock',
-        beats: 'scissors',
-    },
-    {
-        input: 'scissors',
-        beats: 'paper',
-    },
-    {
-        input: 'paper',
-        beats: 'rock',
-    }
-]
+const selections = ['rock', 'paper', 'scissors']
 
 
 //random choice generator for computer
-
 function getComputerChoice() {
     const selectIndex = Math.floor(Math.random() * selections.length);
     return selections[selectIndex]
 }
 
-console.log(getComputerChoice())
-
-//click even based on what u click
-
+//click event based on what the selection
 const buttons = document.querySelectorAll('button')
 
 buttons.forEach((button) => {
     button.addEventListener('click', e => {
         const userInput = button.dataset.selection
+        let computerSelection = getComputerChoice()
+        console.log('pc: ', computerSelection)
+        console.log('user: ', userInput)
+        console.log(playRound(userInput, computerSelection))
     })
 })
 
+//function to determine who wins who loses
 
-
-// function getComputerChoice() {
-//     let randomNumber = Math.floor(Math.random() * 3)
-
-//     if (randomNumber == 0) {
-//         return "rock"
-//     } else if (randomNumber == 1) {
-//         return "paper"
-//     } else {
-//         return "scissors"
-//     }
-// }
-
-// //player selector from buttons
-
-// const buttons = document.querySelectorAll('[data-selection]')
-
-// buttons.forEach(buttons => {
-//     buttons.addEventListener('click', e => {
-//         playerSelection = buttons.dataset.selection
-//         console.log(playRound(playerSelection, getComputerChoice()))
-//         console.log(getComputerChoice())
-//     })
-// })
-
-
-
-// //rounds of RPS with return statements on W/L/T
-// function playRound(playerSelection, computerSelection) {
+function playRound(userInput, computerSelection) {
     
+    const winMessage = 'You win!';
+    const loseMessage = 'You lose!';
+    const tieMessage = 'It\'s a tie!'; 
 
-//     if (playerSelection === computerSelection) {
-//         return "Nobody wins; it\'s a tie!"
-//     } else if (playerSelection === "rock" && computerSelection === "scissors") {
-//         return "You win!"
-//     } else if (playerSelection === "rock" && computerSelection === "paper") {
-//         return "You lose!"
-//     } else if (playerSelection === "paper" && computerSelection === "rock") {
-//         return "You win!"
-//     } else if (playerSelection === "paper" && computerSelection === "scissors") {
-//         return "You lose!"
-//     } else if (playerSelection === "scissors" && computerSelection === "paper") {
-//         return "You win!"
-//     } else if (playerSelection === "scissors" && computerSelection === "rock") {
-//         return "You lose!"
-//     } else {
-//         return "What happened..."
-//     }
-// }
+    // const scoreboard = document.getElementById('scoreboard')
+    
+    if (userInput === computerSelection) {
+        return tieMessage
+    } else if (userInput === 'rock' && computerSelection === 'scissors' ||
+               userInput === 'paper' && computerSelection === 'rock' ||
+               userInput === 'scissors' && computerSelection === 'paper') {
+        return winMessage
+    } else {
+        return loseMessage
+    }
+}
