85ec7200d2d6371990682186f6897fe935fdacea
Joshua-Gabriel Camacho <shua.camacho@gmail.com>
2022-12-09 21:52:35 -0500

    Add function so that scoreboard updates with wins

--- logic.js
+++ logic.js
@@ -3,6 +3,11 @@ const selections = ['rock', 'paper', 'scissors']
 const buttons = document.querySelectorAll('button')
 const scoreboard = document.querySelector('#scoreboard')
 const scoreboardContent = document.createElement('div')
+const userScore = document.querySelector('#userScore')
+const cpuScore = document.querySelector('#cpuScore')
+const results = document.querySelector('.results')
+let iUser = 0;
+let iCPU = 0;
 
 //random choice generator for computer
 function getComputerChoice() {
@@ -15,17 +20,39 @@ buttons.forEach((button) => {
     button.addEventListener('click', e => {
         const userInput = button.dataset.selection
         let computerSelection = getComputerChoice()
+        let outcomeRound = playRound(userInput, computerSelection)
         console.log('pc: ', computerSelection) //change these into divs that will update as the games get played
         console.log('user: ', userInput)
-        playRound(userInput, computerSelection)
+        playFive(outcomeRound)
+        
     })
 });
 
+//function that plays the game to 5
+function playFive(outcomeRound) {
+
+    do {
+        if (outcomeRound === 'It\'s a tie!') {
+            return results.innerText = `${iUser} - ${iCPU}` 
+        } else if (outcomeRound === 'You win!') {
+            ++iUser;
+            // return userScore.innerText = `${iUser}`;
+            return results.innerText = `${iUser} - ${iCPU}` 
+        } else if (outcomeRound === 'You lose!'){
+            ++iCPU;
+            // return cpuScore.innerText = `${iCPU}`;
+            return results.innerText = `${iUser} - ${iCPU}` 
+        } 
+    } while (iUser < 5 && iCPU < 5)
+}
+
+
+//function that plays the round to a winner
 function playRound(userInput, computerSelection) {
     
     const winMessage = 'You win!';
     const loseMessage = 'You lose!';
-    const tieMessage = 'It\'s a tie!'; 
+    const tieMessage = "It\'s a tie!"; 
     
         if (userInput === computerSelection) {
             return scoreboard.innerText = tieMessage;
